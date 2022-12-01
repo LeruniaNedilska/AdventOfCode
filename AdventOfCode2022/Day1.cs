dba02acc476d6ba6b979777145884ace464f3d2f
Valeriia Nedilska <valeriian@mwstechnology.com>
2022-12-01 19:24:37 +0200

    day 1

---
+++ AdventOfCode2022/Day1.cs
@@ -0,0 +1,19 @@
+﻿namespace AdventOfCode2022 {
+    internal class Day1 {
+        public static IEnumerable<int> GetMaxCaloriesSums() {
+            string[] lines = File.ReadAllLines(@"inputs\day1.txt");
+            List<int> sums = new();
+            int tmpSum = 0;
+            foreach (string line in lines) {
+                if (string.IsNullOrWhiteSpace(line)) {
+                    sums.Add(tmpSum);
+                    tmpSum = 0;
+                    continue;
+                }
+
+                tmpSum += int.Parse(line);
+            }
+            return sums.OrderByDescending(x => x).Take(3);
+        }
+    }
+}
---
+++ AdventOfCode2022/Program.cs
@@ -0,0 +1,5 @@
+﻿using AdventOfCode2022;
+
+var day1Result = Day1.GetMaxCaloriesSums();
+Console.WriteLine($"Puzzle 1: {day1Result.First()}");
+Console.WriteLine($"Puzzle 2: {day1Result.Sum()}");
