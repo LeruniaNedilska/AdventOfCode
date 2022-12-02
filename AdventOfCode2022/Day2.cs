2d81d0a9373ae74e739fe6dbe0608ebef6f6f8aa
Valeriia Nedilska <valeriian@mwstechnology.com>
2022-12-02 08:58:26 +0200

    day 2

---
+++ AdventOfCode2022/Day2.cs
@@ -0,0 +1,37 @@
+﻿namespace AdventOfCode2022 {
+    internal class Day2 {
+        public static GameStrategy Puzzle1 = new GameStrategy() {
+            UnconditionalPoints = new Dictionary<string, int>() {
+                {"X", 1 }, {"Y", 2 }, {"Z", 3 }
+            },
+            ConditionalPoints = new Dictionary<string, Dictionary<string, int>>(){
+                { "A" ,new Dictionary<string, int> {{ "X", 3 }, { "Y",  6 }, {"Z", 0 } } },
+                { "B" ,new Dictionary<string, int> {{ "X", 0 }, { "Y",  3 }, {"Z", 6 } } },
+                { "C" ,new Dictionary<string, int> {{ "X", 6 }, { "Y",  0 }, {"Z", 3 } } },
+            },
+        };
+
+        public static GameStrategy Puzzle2 = new GameStrategy() {
+            UnconditionalPoints = new Dictionary<string, int>() {
+                {"X", 0 }, {"Y", 3 }, {"Z", 6 }
+            },
+            ConditionalPoints = new Dictionary<string, Dictionary<string, int>>(){
+                { "A" ,new Dictionary<string, int> {{ "X", 3 }, { "Y",  1 }, {"Z", 2 } } },
+                { "B" ,new Dictionary<string, int> {{ "X", 1 }, { "Y",  2 }, {"Z", 3 } } },
+                { "C" ,new Dictionary<string, int> {{ "X", 2 }, { "Y",  3 }, {"Z", 1 } } },
+            },
+        };
+
+        public static int GetGameScore(GameStrategy strategy) {
+            return File.ReadAllLines(@"inputs\day2.txt").Select(line => {
+                string[] round = line.Split(' ');
+                return strategy.UnconditionalPoints[round[1]] + strategy.ConditionalPoints[round[0]][round[1]];
+            }).Sum();
+        }
+    }
+
+    internal class GameStrategy {
+        public Dictionary<string, int> UnconditionalPoints;
+        public Dictionary<string, Dictionary<string, int>> ConditionalPoints;
+    }
+}
--- AdventOfCode2022/Program.cs
+++ AdventOfCode2022/Program.cs
@@ -3,3 +3,6 @@
 var day1Result = Day1.GetMaxCaloriesSums();
 Console.WriteLine($"Puzzle 1: {day1Result.First()}");
 Console.WriteLine($"Puzzle 2: {day1Result.Sum()}");
+
+Console.WriteLine($"Puzzle 3: {Day2.GetGameScore(Day2.Puzzle1)}");
+Console.WriteLine($"Puzzle 4: {Day2.GetGameScore(Day2.Puzzle2)}");
