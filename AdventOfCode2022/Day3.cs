68bd4a7538196852b51aaacbd7864f8eb1764bbc
Valeriia Nedilska <valeriian@mwstechnology.com>
2022-12-03 08:45:41 +0200

    day 3

---
+++ AdventOfCode2022/Day3.cs
@@ -0,0 +1,35 @@
+﻿namespace AdventOfCode2022 {
+    internal class Day3 {
+        private static readonly string[] input = File.ReadAllLines(@"inputs\day3.txt");
+
+        public static int GetMisplacedItemsSum() {
+            return input.Select(line => {
+                var item = GetCommonItem(new[] { line.Substring(0, line.Length / 2), line.Substring(line.Length / 2) });
+                return GetItemPriority(item);
+            }).Sum();
+        }
+
+        public static int GetBadgeItemsSum() {
+            var groupCount = input.Length / 3;
+            var sum = 0;
+            for (var i = 0; i < groupCount; i++) {
+                var item = GetCommonItem(input.Skip(i*3).Take(3).ToArray());
+                sum += GetItemPriority(item);
+            }
+            return sum;
+        }
+
+        private static char GetCommonItem(string[] compartments) {
+            var charHashSet = compartments[0].ToHashSet();
+            for (var i = 1; i < compartments.Length; i++) {
+                charHashSet.IntersectWith(compartments[i]);
+            }
+
+            return charHashSet.Single();          
+        }
+
+        private static int GetItemPriority(char item) {
+            return char.IsLower(item) ? item - 'a' + 1 : item - 'A' + 27;
+        }
+    }
+}
--- AdventOfCode2022/Program.cs
+++ AdventOfCode2022/Program.cs
@@ -6,3 +6,6 @@ Console.WriteLine($"Puzzle 2: {day1Result.Sum()}");
 
 Console.WriteLine($"Puzzle 3: {Day2.GetGameScore(Day2.Puzzle1)}");
 Console.WriteLine($"Puzzle 4: {Day2.GetGameScore(Day2.Puzzle2)}");
+
+Console.WriteLine($"Puzzle 5: {Day3.GetMisplacedItemsSum()}");
+Console.WriteLine($"Puzzle 6: {Day3.GetBadgeItemsSum()}");
